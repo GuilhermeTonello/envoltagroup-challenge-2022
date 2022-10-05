e2e8bf896272dd5bddcdadd156f3e3edf1b2de79
Guilherme <guitonello@outlook.com.br>
2022-10-05 20:46:47 -0300

    fix: Fixing cors configuration

--- src/main/java/com/envoltagroup/energy_management/configurations/security/CorsConfiguration.java
+++ src/main/java/com/envoltagroup/energy_management/configurations/security/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                    .allowedOrigins("*")
+                    .allowedOriginPatterns("*")
                     .allowedMethods("*")
                     .maxAge(3600L)
                     .allowedHeaders("*")
